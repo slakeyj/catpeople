81efb3e9ed23afbc5f2d3516de8a6a8bf6657e26
Susanna Lakey <zannajune@gmail.com>
2019-05-22 15:22:52 -0700

    Created new javascript and css files

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var name = prompt("Welome to Cat People!  May I have your cat's name?");
+
+if (name === "Tucker") {
+  document.write("Welcome " + name + ", that's my cat's name too!")
+} else if (name === "Abby") {
+  document.write("Welcome " + name + ", that's my cat's name too!")
+} else if (name === "Maggie") {
+  document.write("Welcome " + name + ", that's my cat's name too!")
+} else {
+  document.write("Welcome " + name + ", what a beautiful name!")
+}
+
+
+
+
+
