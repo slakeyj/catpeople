d2f05e200631b5c92c19d930f95ee656c97d24a6
Susanna Lakey <zannajune@gmail.com>
2019-05-23 13:01:12 -0700

    Refactored javascript to create a function.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,28 @@
-alert("Hello and welcome to Cat People!  My apologies for all the pop-ups!")
+// alert("Hello and welcome to Cat People!  My apologies for all the pop-ups!")
 
-var name = prompt("May I have your cat's name?");
 
-if (name.toLowerCase() === "tucker") {
-  document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
-} else if (name.toLowerCase() === "abby") {
-  document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
-} else if (name.toLowerCase() === "maggie") {
-  document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
-} else if (name === null || name === "") {
-  document.write("Welcome to Cat People!");
-} else {
-  document.write("Welcome! We promise we will meet all of " + name + "'s needs.")
+function catGreeting() {
+  var name = prompt("May I have your cat's name?");
+  var matchingNameGreeting = name + "'s my cats name too! ";
+  var output;
+  matchingNameGreeting += "We promise we will meet all of " + name + "'s needs.";
+  if (name.toLowerCase() === "tucker" || name.toLowerCase() === "abby" || name.toLowerCase() === "maggie") {
+    output = matchingNameGreeting;
+    // } else if (name.toLowerCase() === "abby") {
+    //   output = matchingNameGreeting;
+    // } else if (name.toLowerCase() === "maggie") {
+    //   output = matchingNameGreeting;
+  } else if (name === null || name === "") {
+    output = "Welcome to Cat People!";
+  } else {
+    output = "Welcome! We promise we will meet all of " + name + "'s needs.";
+  }
+  return output;
 }
 
-confirm("Press OK to proceed!");
+
+
+// confirm("Press OK to proceed!");
 
 
 
