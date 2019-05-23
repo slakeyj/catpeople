a6961aded800b5baa631f373bbdf12c7272bc1a8
Susanna Lakey <zannajune@gmail.com>
2019-05-22 19:23:59 -0700

    Updated photos and changed header

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 var name = prompt("Welome to Cat People!  May I have your cat's name?");
 
 if (name.toLowerCase() === "tucker") {
-  document.write(name + " 's my cats name too! We promise we will meet all of " + name + "'s needs.")
+  document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
 } else if (name.toLowerCase() === "abby") {
-  document.write(name + " 's my cats name too! We promise we will meet all of " + name + "'s needs.")
+  document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
 } else if (name.toLowerCase() === "maggie") {
-  document.write(name + " 's my cats name too! We promise we will meet all of " + name + "'s needs.")
+  document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
 } else if (name === null || name === "") {
   document.write("Welcome to Cat People!");
 } else {
