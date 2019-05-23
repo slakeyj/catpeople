3d6a639e2d2bc24e2cb228d3d50f97d4550b214a
Susanna Lakey <zannajune@gmail.com>
2019-05-22 17:13:15 -0700

    Created new javascript and css files

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 var name = prompt("Welome to Cat People!  May I have your cat's name?");
 
-if (name === "Tucker") {
+if (name.toLowerCase() === "tucker") {
   document.write("Welcome " + name + ", that's my cat's name too!")
-} else if (name === "Abby") {
+} else if (name.toLowerCase() === "abby") {
   document.write("Welcome " + name + ", that's my cat's name too!")
-} else if (name === "Maggie") {
+} else if (name.toLowerCase() === "maggie") {
   document.write("Welcome " + name + ", that's my cat's name too!")
+} else if (name === null || name === "") {
+  document.write("Welcome to Cat People!");
 } else {
   document.write("Welcome " + name + ", what a beautiful name!")
 }
