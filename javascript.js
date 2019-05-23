9331443dbc49c962d9d6406768d619a24e565657
Susanna Lakey <zannajune@gmail.com>
2019-05-22 20:27:35 -0700

    Added an alert and confirm to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-var name = prompt("Welome to Cat People!  May I have your cat's name?");
+alert("Hello and welcome to Cat People!  My apologies for all the pop-ups!")
+
+var name = prompt("May I have your cat's name?");
 
 if (name.toLowerCase() === "tucker") {
   document.write(name + "'s my cats name too! We promise we will meet all of " + name + "'s needs.")
@@ -11,6 +13,8 @@ if (name.toLowerCase() === "tucker") {
 } else {
   document.write("Welcome! We promise we will meet all of " + name + "'s needs.")
 }
+
+confirm("Press OK to proceed!");
 
 
 
